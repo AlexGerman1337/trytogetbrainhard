0149e649902f45f91edef96ef98b00de3f034edf
Alexander.German <questlogilt@mail.ru>
2020-03-28 16:57:21 +0300

    сейв

---
+++ hard.js
@@ -0,0 +1,12 @@
+
+    let num = 266219;
+
+    function a(number) {
+        return [...number.toString()].reduce((b, c) => b * c);
+}
+//я типо понимаю как это работает, но не понимаю)) (я тупой)    
+     console.log(a(num));
+
+     let num2 = 1296**3;
+     console.log(String(num2).slice(0,2));
+     
